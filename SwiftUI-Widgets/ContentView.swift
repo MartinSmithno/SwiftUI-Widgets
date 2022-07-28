c6005fcac1318146b56e2bfe2e40fb96280e31dc
= <=>
2022-07-28 14:44:43 +0200

    Added PickerControlView

--- SwiftUI-Widgets/ContentView.swift
+++ SwiftUI-Widgets/ContentView.swift
@@ -8,43 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var textInTextField: String = ""
-    @State private var isEditable: Bool = false
+    
     var body: some View {
+    
+        NavigationView {
+            VStack (spacing:30) {
+                
+                NavigationLink("Picker", destination: PickerControlsView()).foregroundColor(.red)
+                NavigationLink("Stepper", destination: PickerControlsView()).foregroundColor(.orange)
+                NavigationLink("Picker", destination: PickerControlsView()).foregroundColor(.yellow)
+                NavigationLink("Picker", destination: PickerControlsView()).foregroundColor(.green)
+                
+            }
+        }
         
-        //Text
-        Text("Lorem Ipsum")
-            .font(.title)
-            .foregroundColor(.orange)
-            .background(.gray)
-        
-        //Button
-        Button(action: {
-            print("Clicked")
-        }, label:{
-            Text("Click here!").font(.title2).foregroundColor(.green)
-        }).buttonStyle(.bordered)
-        
-        //TextField
-        TextField("roundedBorderStyle", text: $textInTextField).padding().textFieldStyle(.roundedBorder)
-        TextField("plainStyle", text: $textInTextField).padding().textFieldStyle(.plain)
-        HStack(spacing: 0){
-            SecureField("secureField AutomaticStyle", text: $textInTextField).padding().textFieldStyle(.automatic)
-            Button(action: {
-                print("Clicked")
-            }, label:{
-                Text("Enter").font(.body).foregroundColor(.green).frame(maxWidth: .infinity, maxHeight: .infinity)
-            }).buttonStyle(.borderedProminent)
-        }.frame(maxWidth: .infinity, maxHeight: 40, alignment: .center)
-        
-        
-        //Toggle
-        Toggle(isOn: $isEditable, label: <#T##() -> _#>)
-        
-        
-        
-        
-        
+
     }
 }
 
